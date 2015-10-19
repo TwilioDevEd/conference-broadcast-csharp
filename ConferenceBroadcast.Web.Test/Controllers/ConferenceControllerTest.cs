ef0de7b790851d00e29f2c37cf34dc4aac44b7b4
Agustin Camino <agustin.camino@gmail.com>
2015-10-19 11:48:48 -0500

    Add Connect action method to Conference controller

--- ConferenceBroadcast.Web.Test/Controllers/ConferenceControllerTest.cs
+++ ConferenceBroadcast.Web.Test/Controllers/ConferenceControllerTest.cs
@@ -19,13 +19,39 @@ namespace ConferenceBroadcast.Web.Test.Controllers
         [Test]
         public void GivenAJoinAction_ThenTheResponseContainsAGatherVerb()
         {
-            var controller = new ConferenceController();
+            var controller = new ConferenceController {Url = Url};
             var result = controller.Join();
 
             result.ExecuteResult(MockControllerContext.Object);
             var document = BuildDocument();
 
-            Assert.That(document.SelectSingleNode("Response/Say"), Is.Not.Null);
+            Assert.That(document.SelectSingleNode("Response/Gather").Attributes["action"].Value,
+                Is.EqualTo("/Conference/Connect"));
+        }
+
+        [TestCase("1", "true", "false", "false")]
+        [TestCase("2", "false", "false", "false")]
+        [TestCase("3", "false", "true", "true")]
+        public void GivenAConnectAction_ThenGeneratesTheAppropriateResponse(
+            string selectedOption,
+            string expectedMute,
+            string expectedStartConferenceOnEnter,
+            string expectedEndConferenceOnEnter)
+        {
+            var controller = new ConferenceController();
+            var result = controller.Connect(selectedOption);
+
+            result.ExecuteResult(MockControllerContext.Object);
+            var document = BuildDocument();
+
+            var conferenceAttributes = document
+                .SelectSingleNode("Response/Dial/Conference").Attributes;
+            Assert.That(conferenceAttributes["muted"].Value,
+                Is.EqualTo(expectedMute));
+            Assert.That(conferenceAttributes["startConferenceOnEnter"].Value,
+                Is.EqualTo(expectedStartConferenceOnEnter));
+            Assert.That(conferenceAttributes["endConferenceOnEnter"].Value,
+                Is.EqualTo(expectedEndConferenceOnEnter));
         }
     }
 }
--- ConferenceBroadcast.Web/Controllers/ConferenceController.cs
+++ ConferenceBroadcast.Web/Controllers/ConferenceController.cs
@@ -18,11 +18,31 @@ namespace ConferenceBroadcast.Web.Controllers
         {
             var response = new TwilioResponse();
             response.Say("You are about to join the Rapid Response conference");
-            response.BeginGather()
+            response.BeginGather(new {action = @Url.Action("Connect")})
                 .Say("Press 1 to join as a listener")
                 .Say("Press 2 to join as a speaker")
                 .Say("Press 3 to join as the moderator")
                 .EndGather();
+
+            return TwiML(response);
+        }
+
+        // GET: Conference/Connect
+        public ActionResult Connect(string digits)
+        {
+            var isMuted = digits.Equals("1"); // Listener
+            var canControlConferenceOnEnter = digits.Equals("3"); // Moderator
+
+            var response = new TwilioResponse();
+            response.Say("You have joined the conference");
+            response.Dial()
+                .DialConference("RapidResponseRoom", new
+                {
+                    waitUrl = "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient",
+                    muted = isMuted,
+                    startConferenceOnEnter = canControlConferenceOnEnter,
+                    endConferenceOnEnter = canControlConferenceOnEnter
+                });
 
             return TwiML(response);
         }
