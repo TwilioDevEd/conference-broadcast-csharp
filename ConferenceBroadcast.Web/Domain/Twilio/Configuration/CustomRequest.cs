dc67111f355f0d437f4c9769843b051537a2d921
David Prothero <david@prothero.com>
2018-04-05 15:15:33 -0700

    Improve the instructions and fix up the code a bit.

--- ConferenceBroadcast.Web/Domain/Twilio/Configuration/CustomRequest.cs
+++ ConferenceBroadcast.Web/Domain/Twilio/Configuration/CustomRequest.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web;
-using System.Web.Configuration;
 
 // ReSharper disable PossibleNullReferenceException
 
@@ -24,14 +23,35 @@ namespace ConferenceBroadcast.Web.Domain.Twilio.Configuration
         /// </summary>
         /// <remarks>
         /// This is a patch used to get the request URL when using ngrok.
-        /// To use ngrok with IIS we had to rewrite the host-header.
+        /// To use ngrok with IIS we had to rewrite the host-header. See:
+        /// https://www.twilio.com/docs/usage/tutorials/how-use-ngrok-windows-and-visual-studio-test-webhooks#get-ngrok-host-name
         /// </remarks>
-        public string Url
+        public string Url => GetProtocol() + "://" + GetDomainAndPort();
+
+        private string GetDomainAndPort()
         {
-            get
+            if (_request.Headers["X-Original-Host"] != null)
             {
-                return _request.Headers.GetValues("Origin")[0];
+                // Assume default port for protocol (http=80, https=443)
+                return _request.Headers["X-Original-Host"];
             }
+
+            // Leave off port if it's the default 80/443
+            if (_request.Url.Port == 80 || _request.Url.Port == 443)
+            {
+                return _request.Url.Host;
+            }
+            return _request.Url.Host + ":" + _request.Url.Port;
+        }
+
+        private string GetProtocol()
+        {
+            if (_request.Headers["X-Forwarded-Proto"] != null)
+            {
+                return _request.Headers["X-Forwarded-Proto"];
+            }
+
+            return _request.IsSecureConnection ? "https" : "http";
         }
     }
 }
